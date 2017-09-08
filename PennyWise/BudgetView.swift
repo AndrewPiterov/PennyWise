63d8ed8f11db5b634cc107153abc5ab683c73686
Andrey Piterov <pfm@live.ru>
2017-09-08 22:57:46 +0300

    BudgetView with coloured bars

--- PennyWise/BudgetView.swift
+++ PennyWise/BudgetView.swift
@@ -22,8 +22,37 @@
 
 import UIKit
 
+@IBDesignable
 class BudgetView: UIView {
 
-  var percent:Float = 0.5
+    var percent:Float = 0.5{
+        didSet{
+            setNeedsDisplay()
+        }
+    }
   
+    @IBInspectable var percentLineColor: UIColor = appGreenColor
+    @IBInspectable var percentWarningLineColor: UIColor = appOrangeColor
+    @IBInspectable var percentDangerLineColor: UIColor = appRedColor
+    
+    @IBInspectable var lineWidth: CGFloat = 13
+    
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath()
+        path.lineWidth = lineWidth
+        path.move(to: CGPoint(x: 0, y: rect.height/2))
+        
+        let end = rect.width*CGFloat(percent)
+        path.addLine(to: CGPoint(x: end, y: rect.height/2))
+        
+        if percent >= 1 {
+            percentDangerLineColor.setStroke()
+        } else if percent >= 0.8{
+            percentWarningLineColor.setStroke()
+        } else {
+            percentLineColor.setStroke()
+        }
+        
+        path.stroke()
+    }
 }
